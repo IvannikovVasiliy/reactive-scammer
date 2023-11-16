e8bec54a5c821d79c76f366a0871441d0c351558
vivannikov1 <vivannikov@neoflex.ru>
2023-11-16 16:47:18 +0300

    add flyway-dependency

--- payment-db-service/src/main/java/ru/neoflex/scammertracking/paymentdb/PersonDbServiceApp.java
+++ payment-db-service/src/main/java/ru/neoflex/scammertracking/paymentdb/PersonDbServiceApp.java
@@ -1,6 +1,5 @@
 package ru.neoflex.scammertracking.paymentdb;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- payment-db-service/src/main/java/ru/neoflex/scammertracking/paymentdb/controller/PaymentController.java
+++ payment-db-service/src/main/java/ru/neoflex/scammertracking/paymentdb/controller/PaymentController.java
@@ -1,9 +1,7 @@
 package ru.neoflex.scammertracking.paymentdb.controller;
 
 import jakarta.validation.Valid;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
