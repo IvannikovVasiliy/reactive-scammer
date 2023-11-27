d161fafac67f7955570fc955e7f6f43c7941bf2c
vivannikov1 <vivannikov@neoflex.ru>
2023-11-27 13:02:27 +0300

    return to push-model kafka

--- analyzer/src/main/java/ru/neoflex/scammertracking/analyzer/config/kafka/KafkaConsumerConfig.java
+++ analyzer/src/main/java/ru/neoflex/scammertracking/analyzer/config/kafka/KafkaConsumerConfig.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.Arrays;
@@ -26,41 +29,21 @@ public class KafkaConsumerConfig {
     @Value(value = "${spring.kafka.topic.payments}")
     private String paymentTopic;
 
-//    @Bean
-//    public ConsumerFactory<String, byte[]> consumerFactory() {
-//        Map<String, Object> props = new HashMap<>();
-//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-//        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
-//        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-////        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 50000000);
-////        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 50000000);
-////        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1000);
-//        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new ByteArrayDeserializer());
-//    }
-
     @Bean
-    public Consumer<String, byte[]> createConsumer() {
+    public ConsumerFactory<String, byte[]> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-//        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 50000000);
-        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 10);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-        KafkaConsumer<String, byte[]> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList(paymentTopic));
-        return consumer;
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new ByteArrayDeserializer());
     }
 
-//    @Bean
-//    public ConcurrentKafkaListenerContainerFactory<String, byte[]> paymentsKafkaListenerContainerFactory() {
-//        var factory = new ConcurrentKafkaListenerContainerFactory<String, byte[]>();
-//        factory.setConsumerFactory(consumerFactory());
-//        return factory;
-//    }
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, byte[]> paymentsKafkaListenerContainerFactory() {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, byte[]>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
 }
--- analyzer/src/main/java/ru/neoflex/scammertracking/analyzer/kafka/consumer/PaymentConsumer.java
+++ analyzer/src/main/java/ru/neoflex/scammertracking/analyzer/kafka/consumer/PaymentConsumer.java
@@ -3,21 +3,18 @@ package ru.neoflex.scammertracking.analyzer.kafka.consumer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.neoflex.scammertracking.analyzer.domain.dto.PaymentRequestDto;
 import ru.neoflex.scammertracking.analyzer.kafka.producer.PaymentProducer;
 import ru.neoflex.scammertracking.analyzer.service.PreAnalyzerPayment;
 
 import java.io.IOException;
-import java.time.Duration;
 
 @Service
 @Slf4j
@@ -25,19 +22,17 @@ import java.time.Duration;
 public class PaymentConsumer {
 
     @Autowired
-    public PaymentConsumer(PreAnalyzerPayment preAnalyzerPayment, PaymentProducer paymentProducer, Consumer consumer) {
+    public PaymentConsumer(PreAnalyzerPayment preAnalyzerPayment, PaymentProducer paymentProducer) {
         this.preAnalyzerPayment = preAnalyzerPayment;
         this.paymentProducer = paymentProducer;
-        this.consumer = consumer;
     }
 
     private final PreAnalyzerPayment preAnalyzerPayment;
     private final PaymentProducer paymentProducer;
-    private final Consumer<String, byte[]> consumer;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    //    @KafkaListener(topics = "${spring.kafka.topic.payments}", containerFactory = "paymentsKafkaListenerContainerFactory")
+    @KafkaListener(topics = "${spring.kafka.topic.payments}", containerFactory = "paymentsKafkaListenerContainerFactory")
     public void consumePayment(@Payload byte[] paymentRequestBytes,
                                @Header(KafkaHeaders.RECEIVED_KEY) String key) {
         log.info("Input consumePayment. Received key={} bytes array", key);
@@ -49,32 +44,5 @@ public class PaymentConsumer {
             log.error("Cannot map input request={} to PaymentRequestDto.class", paymentRequestBytes);
             paymentProducer.sendSuspiciousMessage(key, paymentRequestBytes);
         }
-    }
-
-    @Scheduled(fixedRate = 1000)
-    public void method() {
-        ConsumerRecords<String, byte[]> records = consumer.poll(Duration.ofSeconds(1));
-        byte[] paymentRequestBytes = null;
-        String key = null;
-
-        try {
-            for (ConsumerRecord<String, byte[]> record : records) {
-                paymentRequestBytes = record.value();
-                key = record.key();
-                PaymentRequestDto paymentRequest = objectMapper.readValue(paymentRequestBytes, PaymentRequestDto.class);
-                preAnalyzerPayment.preAnalyzeConsumeMessage(key, paymentRequest);
-            }
-        } catch (IOException e) {
-            log.error("Cannot map input request={} to PaymentRequestDto.class", paymentRequestBytes);
-            paymentProducer.sendSuspiciousMessage(key, paymentRequestBytes);
-        }
-
-//        ConsumerRecords<String, byte[]> records = consumer.poll(Duration.ofSeconds(1));
-//        for (ConsumerRecord<String, byte[]> rec : records) {
-//            System.out.println(rec.value());
-//        }
-//        System.out.println("------------");
-
-        // close
     }
 }
